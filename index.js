1a401f7024719da1aeb472de9c2c4d5ba2059950
sejin <sejinnnnnn@naver.com>
2020-11-10 18:17:50 +0900

    e

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var express = require('express');
+var mysql = require('mysql');
 
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   console.log(req, res);
   res.send('Hello World!');
 });
 
 var port = 49953; // AGGRE 제일 닮은 번호임 ㅋㅋ
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log('server on http://localhost:' + port);
 });
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  
+})
