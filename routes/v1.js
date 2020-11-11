f6acb3993bf442d243c4adbcc2bf064457d222eb
sejin <sejinnnnnn@naver.com>
2020-11-11 19:52:42 +0900

    es

--- routes/v1.js
+++ routes/v1.js
@@ -1,2 +1,7 @@
 const express = require('express');
+const math = require('mathjs');
+const router = express.Router();
 
+router.post('/model/global/update', verifyTokenGlobal, async (req, res) => {
+
+});
