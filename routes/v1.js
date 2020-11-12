d9c3626aae0bdd88b63a9404f004ec052b83592d
sejinnnnnn <sejinnnnnn@naver.com>
2020-11-12 14:17:34 +0900

    wfeqwfeq

--- routes/v1.js
+++ routes/v1.js
@@ -1,2 +1,12 @@
 const express = require('express');
+const { verifyTokenMiddle } = require('./middleware');
+
+const router = express.Router();
+
+
+
+router.post('/__protocol/aggregation', verifyTokenMiddle ,async (req, res) => {
+    const { party_id } = req.body;
+    
+});
 
